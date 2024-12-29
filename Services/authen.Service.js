45a735afdb0e0beeed0b95b0a90449caaa04469a
Dinh Tran <tvandinh93@gmail.com>
2024-12-29 19:13:21 +0700

    add bcrypt lib, implement login method for auth service, implement create access/refresh token

--- Services/authen.Service.js
+++ Services/authen.Service.js
@@ -1,8 +1,38 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("brypt");
+
+const SALT_ROUNDS = 10; // cost factor
 
 class authentication {
-    async login() {}
-    async createToken(userId, role) {}
+    async login(data) {
+        const { username, password } = data;
+        if (!(await isExsitedUser(username))) {
+            return { status: 404, message: "User not found" };
+        }
+        const { hashedPassword, userId, role } = await getUserInfo(username);
+        if (await bcrypt.compare(password, hashedPassword)) {
+            return { status: 401, message: "Password is incorrect" };
+        }
+        return {
+            status: 200,
+            message: {
+                message: "Login successful",
+                accessToken: this.createAccessToken(userId, role),
+                refreshToken: this.createRefreshToken(userId, role),
+            },
+        };
+    }
+    async register(data) {}
+    createAccessToken(userId, role) {
+        return jwt.sign({ userId, role }, process.env.SECRET_KEY, {
+            expiresIn: "3m",
+        });
+    }
+    createRefreshToken(userId, role) {
+        return jwt.sign({ userId, role }, process.env.SECRET_KEY, {
+            expiresIn: "1h",
+        });
+    }
     validate(token) {
         try {
             const decoded = jwt.verify(token, process.env.SECRET_KEY);
@@ -11,6 +41,8 @@ class authentication {
             return { isSuccess: false, decoded: err };
         }
     }
+    async isExsitedUser(username) {}
+    async getUserInfo(username) {}
 }
 
 module.exports = new authentication();
