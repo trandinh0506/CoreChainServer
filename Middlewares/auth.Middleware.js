1915d8727850c964721db9c1c058b87c3f48e539
Dinh Tran <tvandinh93@gmail.com>
2025-01-27 07:03:40 +0700

    skip for login and register path

--- Middlewares/auth.Middleware.js
+++ Middlewares/auth.Middleware.js
@@ -1,4 +1,8 @@
 module.exports.requireAuth = (req, res, next) => {
+    const skipRoutes = ["/auth/login", "/auth/register"];
+    if (skipRoutes.includes(req.path)) {
+        return next();
+    }
     const token =
         req.headers.authorization?.split(" ")[1] || req.cookies?.token;
     if (!token) {
