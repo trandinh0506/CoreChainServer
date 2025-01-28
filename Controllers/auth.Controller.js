40a7172e4833919e06ee0141fe85965e6cd980f8
Dinh Tran <tvandinh93@gmail.com>
2025-01-28 15:38:26 +0700

    change response in login

--- Controllers/auth.Controller.js
+++ Controllers/auth.Controller.js
@@ -1,10 +1,26 @@
 const authService = require("../Services/authen.Service");
-
+const ACCESS_TOKEN_MAX_AGE = 3 * 60 * 1000;
+const REFRESH_TOKEN_MAX_AGE = 60 * 60 * 1000;
 class authController {
     async login(req, res) {
         const data = req.body.data;
         const result = await authService.login(data);
         console.log(result);
+        if (result.status === 200) {
+            res.cookie("accessToken", result.message.accessToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "Strict",
+                maxAge: ACCESS_TOKEN_MAX_AGE,
+            });
+            res.cookie("refreshToken", result.message.refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "Strict",
+                maxAge: REFRESH_TOKEN_MAX_AGE,
+            });
+            res.status(result.status).json(result.message.message);
+        }
         res.status(result.status).json(result.message);
     }
     async register(req, res) {
