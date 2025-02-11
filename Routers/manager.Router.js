4bb21037843e77e19c83a5429a90debd80604e45
Dinh Tran <tvandinh93@gmail.com>
2025-02-11 20:38:29 +0700

    add validate admin route

--- Routers/manager.Router.js
+++ Routers/manager.Router.js
@@ -3,6 +3,14 @@ const managerController = require("../Controllers/manager.Controller");
 
 const managerRouter = express.Router();
 
+router.use((req, res, next) => {
+    const decoded = req.user;
+    if (decoded.role !== "manager") {
+        res.status(403).json({ message: "Forbidden denied" });
+    }
+    next();
+});
+
 managerRouter.post("/tasks", managerController.allocateTasks);
 
 module.exports = managerRouter;
