35ee8a48523e7febb71ae1800c1f120940d11412
Dinh Tran <tvandinh93@gmail.com>
2025-02-11 20:44:41 +0700

    fix manager route error

--- Routers/manager.Router.js
+++ Routers/manager.Router.js
@@ -3,7 +3,7 @@ const managerController = require("../Controllers/manager.Controller");
 
 const managerRouter = express.Router();
 
-router.use((req, res, next) => {
+managerRouter.use((req, res, next) => {
     const decoded = req.user;
     if (decoded.role !== "manager") {
         res.status(403).json({ message: "Forbidden denied" });
