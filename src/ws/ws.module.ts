273dc4fcdaf88468fb5f61796916b21f0ca77d08
Dinh Tran <tvandinh93@gmail.com>
2025-03-12 16:04:59 +0700

    feat: create base ws module

---
+++ src/ws/ws.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { WsGateway } from './ws.gateway';
+import { WsService } from './ws.service';
+
+@Module({
+  providers: [WsGateway, WsService],
+  exports: [WsService],
+})
+export class WsModule {}
