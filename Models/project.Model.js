d676e056b5d19234b1cdba6ccff13fe14f714ab2
Dinh Tran <tvandinh93@gmail.com>
2025-03-12 13:20:19 +0700

    reset main to empty

--- Models/project.Model.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const projectSchema = require("../Schemas/project.Schema");
-const projectModel = mongoose.model("projects", projectSchema);
-
-module.exports = projectModel;
--- Schemas/candidate.Schema.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const candidateSchema = new mongoose.Schema({
-    fulName: String,
-});
-
-module.exports = candidateSchema;
