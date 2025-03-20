770e10632156092b6dd93679b7f6f1145c227636
Dinh Tran <tvandinh93@gmail.com>
2025-03-20 11:16:03 +0700

    add chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,36 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { BlockchainModule } from './blockchain/blockchain.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+        connectionFactory: (connection) => {
+          connection.plugin(softDeletePlugin);
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    //throttler limit in 60 seconds, maximum 10 requests
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 10,
+          ttl: 60 * 1000,
+        },
+      ],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    BlockchainModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
