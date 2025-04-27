adf9975840181703e101d938cafa847fc6ee1d3e
Tri Ngoc <caotringoc11@gmail.com>
2025-04-27 13:10:07 +0700

    add favicon

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,6 +15,11 @@ export class AppController {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HRM API | Human Resource Management System</title>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://res.cloudinary.com/daq721xar/image/upload/v1745734103/favicon_ilqcou.png"
+        />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <style>
