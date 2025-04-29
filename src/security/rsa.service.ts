3de78d90c772a792baa2c750a7be84dc2a9c67a3
Tri Ngoc <caotringoc11@gmail.com>
2025-04-29 18:41:45 +0700

    modified get keys

--- src/security/rsa.service.ts
+++ src/security/rsa.service.ts
@@ -20,15 +20,92 @@ export class RsaService {
     // Create keys directory if it doesn't exist
     // Load existing keys
     try {
-      this.privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
-      this.publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
+      this.privateKey = fs.readFileSync(this.privateKeyPath, 'utf8');
+      this.publicKey = fs.readFileSync(this.publicKeyPath, 'utf8');
       if (!this.privateKey || !this.publicKey) {
-        throw new Error('Cannot get RSA Key !');
+        this.initKeyFiles();
       }
-    } catch(error) {
-      Logger.log('Error read keys: ', error);
+      // this.privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
+      // this.publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
+      // console.log(this.privateKeyPath);
+      // console.log(this.publicKeyPath);
+      // console.log(this.privateKey);
+      // console.log(this.publicKey);
+    } catch (error) {
+      console.log('Error read keys: ', error);
+      this.initKeyFiles();
+      // throw error;
     }
   }
+
+  private initKeyFiles() {
+    const keysDir = path.join(process.cwd(), 'keys');
+    if (!fs.existsSync(keysDir)) {
+      fs.mkdirSync(keysDir);
+    }
+
+    const privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
+    const publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
+
+    if (!privateKey && !publicKey) {
+      this.generateKeyPairRSA();
+      throw new Error('Cannot find RSA Keys !');
+    }
+
+    if (privateKey) {
+      fs.writeFileSync(
+        path.join(keysDir, 'private.pem'),
+        Buffer.from(privateKey, 'base64').toString('utf-8'),
+        { flag: 'w' },
+      );
+    }
+
+    if (publicKey) {
+      fs.writeFileSync(
+        path.join(keysDir, 'public.pem'),
+        Buffer.from(publicKey, 'base64').toString('utf-8'),
+        { flag: 'w' },
+      );
+    }
+  }
+
+  private generateKeyPairRSA() {
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+      },
+    });
+
+    fs.writeFileSync(this.privateKeyPath, privateKey);
+    fs.writeFileSync(this.publicKeyPath, publicKey);
+
+    // Encrypt secret key and save to .env file
+    // will be implemented after considering security capabilities
+
+    // const secretKey = this.configService.get<string>('SECRET_KEY');
+    // const encryptedSecretKey = this.encryptSecretKey(secretKey);
+    // this.configService.set('ENCRYPTED_SECRET_KEY', encryptedSecretKey);
+  }
+
+  // private initializeKeys() {
+  //   // Create keys directory if it doesn't exist
+  //   // Load existing keys
+  //   try {
+  //     this.privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
+  //     this.publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
+  //     if (!this.privateKey || !this.publicKey) {
+  //       throw new Error('Cannot get RSA Key !');
+  //     }
+  //   } catch(error) {
+  //     Logger.log('Error read keys: ', error);
+  //   }
+  // }
 
   encryptSecretKey(secretKey: string): string {
     const buffer = Buffer.from(secretKey, 'utf8');
@@ -38,84 +115,7 @@ export class RsaService {
   decryptSecretKey(encryptedSecretKey: string): string {
     const buffer = Buffer.from(encryptedSecretKey, 'base64');
     const decrypted = crypto.privateDecrypt(this.privateKey, buffer);
+    console.log(decrypted.toString('utf8'));
     return decrypted.toString('utf8');
   }
-
-  // private initializeKeys() {
-  //   // Create keys directory if it doesn't exist
-  //   // Load existing keys
-  //   try {
-  //     this.privateKey = fs.readFileSync(this.privateKeyPath, 'utf8');
-  //     this.publicKey = fs.readFileSync(this.publicKeyPath, 'utf8');
-
-  //     if (!this.privateKey || !this.publicKey) {
-  //       this.initKeyFiles();
-  //     }
-  //     // this.privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
-  //     // this.publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
-  //     // console.log(this.privateKeyPath);
-  //     // console.log(this.publicKeyPath);
-  //     // console.log(this.privateKey);
-  //     // console.log(this.publicKey);
-  //   } catch(error) {
-  //     console.log('Error read keys: ', error);
-  //     this.initKeyFiles();
-  //     // throw error;
-  //   }
-  // }
-
-  // private initKeyFiles() {
-  //   const keysDir = path.join(process.cwd(), 'keys');
-  //   if (!fs.existsSync(keysDir)) {
-  //     fs.mkdirSync(keysDir);
-  //   }
-
-  //   const privateKey = this.configService.get<string>('RSA_PRIVATE_KEY');
-  //   const publicKey = this.configService.get<string>('RSA_PUBLIC_KEY');
-
-  //   if (!privateKey && !publicKey) {
-  //     this.generateKeyPairRSA();
-  //     throw new Error('Cannot find RSA Keys !');
-  //   }
-
-  //   if (privateKey) {
-  //     fs.writeFileSync(
-  //       path.join(keysDir, 'private.pem'),
-  //       Buffer.from(privateKey, 'base64').toString('utf-8'),
-  //       { flag: 'w' },
-  //     );
-  //   }
-
-  //   if (publicKey) {
-  //     fs.writeFileSync(
-  //       path.join(keysDir, 'public.pem'),
-  //       Buffer.from(publicKey, 'base64').toString('utf-8'),
-  //       { flag: 'w' },
-  //     );
-  //   }
-  // }
-
-  // private generateKeyPairRSA() {
-  //   const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-  //     modulusLength: 2048,
-  //     publicKeyEncoding: {
-  //       type: 'spki',
-  //       format: 'pem',
-  //     },
-  //     privateKeyEncoding: {
-  //       type: 'pkcs8',
-  //       format: 'pem',
-  //     },
-  //   });
-
-  //   fs.writeFileSync(this.privateKeyPath, privateKey);
-  //   fs.writeFileSync(this.publicKeyPath, publicKey);
-
-  //   // Encrypt secret key and save to .env file
-  //   // will be implemented after considering security capabilities
-
-  //   // const secretKey = this.configService.get<string>('SECRET_KEY');
-  //   // const encryptedSecretKey = this.encryptSecretKey(secretKey);
-  //   // this.configService.set('ENCRYPTED_SECRET_KEY', encryptedSecretKey);
-  // }
 }
